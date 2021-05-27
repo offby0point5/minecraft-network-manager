0b94da25e22d17078f41df2a4a3ede9553f7e55e
offbyone <offbyone@disroot.org>
2021-05-27 15:38:16 +0200

    Make server name field static in ServermanagerPaper

--- servermanager-paper/src/main/java/com/github/offby0point5/mc/plugin/hybrid/servermanager/ServermanagerPaper.java
+++ servermanager-paper/src/main/java/com/github/offby0point5/mc/plugin/hybrid/servermanager/ServermanagerPaper.java
@@ -1,24 +1,25 @@
 package com.github.offby0point5.mc.plugin.hybrid.servermanager;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ServermanagerPaper extends JavaPlugin {
-    public final String serverName = "server-"+this.getServer().getPort();
+    public static final String serverName = "server-"+ Bukkit.getServer().getPort();
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         this.getServer().getPluginManager().registerEvents(new ServerMenu(), this);
-        ProxyApi.putServerPorts(this.serverName,
+        ProxyApi.putServerPorts(serverName,
                 new ServerPorts(this.getServer().getPort(), null, null));
-        ProxyApi.putServerGroups(this.serverName,  // todo add this to a config
+        ProxyApi.putServerGroups(serverName,  // todo add this to a config
                 new ServerGroups("lobby", "random", "foyer"));
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-        ProxyApi.deleteServer(this.serverName);
+        ProxyApi.deleteServer(serverName);
         ProxyApi.shutdown();
     }
 }
