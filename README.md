f676e86e4a4cf412a263e447571e8326053dc90c
offbyone <offbyone@disroot.org>
2021-06-15 00:34:17 +0200

    Add README.md

---
+++ README.md
@@ -0,0 +1,13 @@
+### Servermanager
+
+This repo is just hacked together by someone who does not know anything about code quality or development strategies.
+
+I simply try to build something that suits my needs for a private network.
+
+With these plugins for proxy (BungeeCord, Velocity) and backend server (Paper, Spigot is not supported) should be able to automatically register and unregister backend servers as they start and stop. So there is no need to configure the whole network in the proxy config.
+
+The current version got a (really bulky) feature of a server menu.
+
+### ToDo
+- [ ] Make a better API for more possible server switching methods
+- [ ] Write better code
--- servermanager-velocity/src/main/java/com/github/offby0point5/mc/plugin/hybrid/servermanager/groups/GroupEvents.java
+++ servermanager-velocity/src/main/java/com/github/offby0point5/mc/plugin/hybrid/servermanager/groups/GroupEvents.java
@@ -6,16 +6,28 @@ import com.velocitypowered.api.event.player.PlayerChooseInitialServerEvent;
 import com.velocitypowered.api.proxy.server.RegisteredServer;
 import net.kyori.adventure.text.Component;
 
-/*
-This class handles events relevant for the server groups
+/**
+ * This class handles events relevant for the server groups
  */
 public class GroupEvents {
+    // todo event when player requests server change
+
+    /**
+     * Execute the KickRule
+     */
     @Subscribe public void onPlayerKicked(KickedFromServerEvent event) {
+        if (event.kickedDuringServerConnect()) return; // do nothing if kicked due to whitelist, version or something
         String serverId = event.getServer().getServerInfo().getName();
         String mainGroupName = ServerData.getServer(serverId).groups.main;
-        ServerGroup group = ServerGroup.getGroup(mainGroupName);
+        ServerGroup group = ServerGroup.getInstance(mainGroupName);  // if group does not exist, create it
         ServerGroup newGroup = group.getKickGroup(event.getPlayer());
         if (newGroup == null) {
+            // todo do not kick from proxy if reason is not server shutdown or restart
+            Component reason = event.getServerKickReason().orElse(Component.empty());
+            if (reason.contains(Component.text("Server is restarting"))
+                    || reason.contains(Component.text("Server closed"))) {
+                event.setResult(KickedFromServerEvent.RedirectPlayer.create(group.getJoinServer(event.getPlayer())));
+            }
             event.setResult(KickedFromServerEvent.DisconnectPlayer.create(
                     event.getServerKickReason().orElse(Component.text("Kicked from server "+serverId+"! (For no reason)"))));
             return;
@@ -25,6 +37,9 @@ public class GroupEvents {
                 event.getServerKickReason().orElse(Component.empty())));
     }
 
+    /**
+     * Always send players to a lobby server on join
+     */
     @Subscribe public void onPlayerJoin(PlayerChooseInitialServerEvent event) {
         ServerGroup lobbyGroup = ServerGroup.getGroup("lobby");
         RegisteredServer server = lobbyGroup.getJoinServer(event.getPlayer());
